e0933d95cf24341c4cb06cd94249bcd21ebfd240
MDz1985 <m_d@tut.by>
2021-08-13 12:49:58 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
   
+  if (n < 0){
+    n = -n;
+}
+let textNumber = n.toString();
+let result = '';
+  for (let i = 0; i < textNumber.length ; i++){
+      result = textNumber[i] + result;
+  }
+
+    return Number(result);
+
 }
